5fd1d89637c29191f5ad4f1fb2c1acb2ee196d41
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-01-14 12:19:07 -0500

    Working on Center Component

--- components/Center.js
+++ components/Center.js
@@ -1,5 +1,35 @@
+import { useSession } from 'next-auth/react';
+import {ChevronDownIcon} from "@heroicons/react/outline";
+import { useEffect } from 'react';
+const colors = [
+    "from-indigo-500",
+    "from-blue-500",
+    "from-green-500",
+    "from-red-500",
+    "from-yellow-500",
+    "from-purple-500",
+];
+
 function Center(){
-    return <div className="flex flex-grow">I am Center</div>
+    const{data:session} = useSession();
+
+    useEffect(() =>{
+
+    },[])
+
+    return <div className="flex-grow">
+        <header className='absolute top-5 right-8'>
+            <div className='flex items-center bg-red-300 space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2'>
+                <img className='rounded-full w-10 h-10' src="gill.jpg" alt={session?.user.image}></img>
+                <h2>{session?.user.name}</h2>
+                <ChevronDownIcon className="h-5 w-5"></ChevronDownIcon>
+            </div>
+        </header>
+        <section className={`flex items-end space-x-7 bg-gradient-to-b to-black from-red-500 h-80 text-white padding-8`}>
+            <img src="" alt=""></img>
+            <h1>Hello</h1>
+        </section>
+    </div>
 }
 
 export default Center
