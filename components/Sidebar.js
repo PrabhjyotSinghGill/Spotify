a2ffd1b41f2063f57b18028e4f86b548aff049ff
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-01-17 13:13:25 -0500

    useSpotify.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,9 +1,21 @@
 import {HomeIcon,SearchIcon,LibraryIcon,PlusCircleIcon,HeartIcon,RssIcon} from "@heroicons/react/outline";
 import {signOut, useSession} from "next-auth/react";
+import{useEffect,useState} from "react";
+import useSpotify from "../hooks/useSpotify.js";
 
 function Sidebar(){
+    const spotifyApi = useSpotify();
     const{data:session, status} = useSession();
-    console.log(session);
+    const [playlists,setPlaylists] = useState([]);
+
+    useEffect(()=>{
+        if(spotifyApi.getAccessToken()){
+            spotifyApi.getUserPlaylists().then((data) =>{
+                setPlaylists(data.body.items);
+            });
+        }
+    },[session,spotifyApi]);
+    
     return<div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen">
        <div className="space-y-4">
        <button className="flex items-center space-x-2 hover:text-white" onClick={()=>signOut()}>
@@ -37,9 +49,9 @@ function Sidebar(){
            </button>
            <hr className="border-t-[0.1px] border-gray-900"></hr>
            {/*Playlists...*/}
-           <p className="cursor-pointer hover:text-white">
-               Playlist name...
-           </p>
+           {playlists.map((playlist) =>(
+               <p key={playlist.id} className="cursor-pointer hover:text-white" >{playlist.name}</p>
+           ))}
        </div>
     </div>
 }
