e0f3a986057abbb5250265c4c66fda717a499424
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-01-17 15:53:18 -0500

    index.tsx converted to index.js

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import Sidebar from "../components/Sidebar.js"
+import Center from "../components/Center.js"
+import { getSession } from 'next-auth/react'
+
+export default function Home() {
+  return (
+    <div className="bg-black h-screen overflow-hidden">
+      <Head>
+        <title>Spotify</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className='flex'>
+        {/*Sidebar*/}
+        <Sidebar></Sidebar>
+        {/*Center*/}
+        <Center></Center>
+      </main>
+      <div>{/*Player*/}</div>
+    </div>
+  )
+}
+
+export async function getServerSideProps(context){
+  const session = await getSession(context);
+  
+  return{
+    props:{
+      session,
+    },
+  };
+}
