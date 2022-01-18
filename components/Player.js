25af62d9a5c2b3d29b67d6fdaf1be9d290fbcf5f
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-01-18 16:27:14 -0500

    Modified Player.js

--- components/Player.js
+++ components/Player.js
@@ -1,4 +1,28 @@
+import {useState} from "react";
+import useSpotify from './../hooks/useSpotify';
+import { useSession } from 'next-auth/react';
+import { useRecoilState } from 'recoil';
+import { currentTrackIdState, isPlayingState} from './../atoms/songAtom';
+import useSongInfo from "../hooks/useSongInfo";
+
 function Player(){
-    return <div>Player</div>
+    const spotifyApi = useSpotify();
+    const {data: session, status} = useSession();
+    const [currentTrackId, setCurrentIdTrack] = useRecoilState(currentTrackIdState);
+    const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+    const [volume, setVolume] = useState(50);
+    const songInfo = useSongInfo();
+
+    return <div>
+        {/*Left*/}
+        <div>
+            <img className="hidden md:inline h-10 w-10" src={songInfo?.album.images?.[0]?.url} alt=""></img>
+        </div>
+        {/*Center*/}
+        <div></div>
+        {/*Right*/}
+        <div></div>
+
+    </div>
 }
 export default Player
